02a8564f6f965a911cbd483373963ec5ef0be7df
Ednardo Gomes <ednardo@email.com>
2022-03-17 20:43:31 -0300

    Criando o arquivo routes e Dashboard.

---
+++ src/routes/index.tsx
@@ -0,0 +1,11 @@
+import { Routes as Switch, Route } from "react-router-dom";
+
+import { Dashboard } from "../pages/Dashboard";
+
+export function Routes() {
+  return (
+    <Switch>
+      <Route path="/" element={Dashboard} />
+    </Switch>
+  );
+}
