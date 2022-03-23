e873a6b7ffada347f7617d8daa34f6afec8bad22
Ednardo Gomes <ednardo@email.com>
2022-03-22 21:50:59 -0300

    Criando o componente Food.

---
+++ src/components/Food/index.tsx
@@ -0,0 +1,82 @@
+import { useState } from "react";
+
+import { FiEdit3, FiTrash } from "react-icons/fi";
+
+import { Container } from "./styles";
+import { api } from "../../services/api";
+
+interface FoodItem {
+  id: number;
+  name: string;
+  description: string;
+  price: number;
+  available: boolean;
+  image: string;
+}
+
+interface FoodPros {
+  food: FoodItem;
+  handleDelete: (id: number) => Promise<void>;
+  handleEditFood: (food: FoodItem) => void;
+}
+
+export function Food({ food, handleDelete, handleEditFood }: FoodPros) {
+  const [isAvailable, setIsAvailable] = useState(food.available);
+
+  async function toggleAvailable() {
+    await api.put(`/food/${food.id}`, {
+      ...food,
+      available: !isAvailable,
+    });
+
+    setIsAvailable(!isAvailable);
+  }
+
+  function setEditingFood() {
+    handleEditFood(food);
+  }
+
+  return (
+    <Container available={isAvailable}>
+      <header>
+        <img src={food.image} alt={food.name} />
+      </header>
+      <section className="body">
+        <h2>{food.name}</h2>
+        <p>{food.description}</p>
+        <p className="price">
+          R$ <b>{food.price}</b>
+        </p>
+      </section>
+      <section className="footer">
+        <div className="icon-container">
+          <button type="button" className="icon" onClick={setEditingFood}>
+            <FiEdit3 size={20} />
+          </button>
+
+          <button
+            type="button"
+            className="icon"
+            onClick={() => handleDelete(food.id)}
+          >
+            <FiTrash size={20} />
+          </button>
+        </div>
+
+        <div className="availability-container">
+          <p>{isAvailable ? "Disponível" : "Indisponível"}</p>
+
+          <label htmlFor={`available-switch-${food.id}`} className="switch">
+            <input
+              id={`available-switch-${food.id}`}
+              type="checkbox"
+              checked={isAvailable}
+              onChange={toggleAvailable}
+            />
+            <span className="slider" />
+          </label>
+        </div>
+      </section>
+    </Container>
+  );
+}
